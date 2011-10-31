9d5681f343f7f772d5aa8c41934359c5ec5b8d44
johannes.93.1337@gmail.com <johannes.93.1337@gmail.com@3a992061-0d19-d50c-7e29-58eca9e919f6>
2011-10-31 13:35:47 +0000

    Konsol knappar

---
+++ Projekt/src/projekt/event/Keys.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt.event;
+
+import java.awt.event.KeyEvent;
+
+/**
+ *
+ * @author johannes
+ */
+public final class Keys {
+	public static final int left = KeyEvent.VK_A;
+	public static final int right = KeyEvent.VK_D;
+	public static final int up = KeyEvent.VK_W;
+	public static final int down = KeyEvent.VK_S;
+	public static final int a=KeyEvent.VK_Z;
+	public static final int b=KeyEvent.VK_X;
+	public static final int select=KeyEvent.VK_1;
+	public static final int esc=KeyEvent.VK_ESCAPE;
+}
