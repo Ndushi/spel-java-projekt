2bc436303e44cf63fb622df4f02727f7bef85432
johannes.93.1337@gmail.com <johannes.93.1337@gmail.com@3a992061-0d19-d50c-7e29-58eca9e919f6>
2011-10-31 13:36:34 +0000

    Bugg av render.Projekt.java :D

--- Projekt/src/render/Projekt.java
+++ Projekt/src/render/Projekt.java
@@ -9,15 +9,15 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import projekt.event.Dialogs;
+import projekt.event.Keys;
+
 /**
  * Projekt är en subklass av Render vilket innebär att det är här all grafik egentligen skrivs ut på skärmen
  * @author johannes
  */
-public class Projekt extends Render{
+public class Projekt extends Render {
+
 	/**
 	 * världens nuvarande egenskaper sparas och ändras i denna variabel.
 	 */
@@ -26,16 +26,18 @@ public class Projekt extends Render{
 	 * focus innehåller information om var karaktären befinner sig och hur världen skall relatera sig till var karaktären är(görs ex med att sätta world.setOffset(int x,int y)
 	 */
 	public character focus;
+
 	/**
 	 * konstruktorn för spelet
 	 */
-	public Projekt(){
-		focus=new character(2,2,radius);
-		String[] s =new String[4];
-		for(int i=0;i<4;i++)
-			s[i]="/res/CharMain/ts"+i+".png";
+	public Projekt() {
+		focus = new character(2, 2, radius);
+		String[] s = new String[4];
+		for (int i = 0; i < 4; i++) {
+			s[i] = "/res/CharMain/ts" + i + ".png";
+		}
 		focus.setChar("/res/CharMain/firehero.gif");
-		this.world=new World(this.getClass().getResource("/res/worlds/world200").getPath());
+		this.world = new World(this.getClass().getResource("/res/worlds/world200").getPath());
 		focus.setOnWalkCallback(new OnWalkCallback() {
 
 			public boolean onWalk() {
@@ -45,178 +47,203 @@ public class Projekt extends Render{
 			public boolean onStartWalk() {
 				return false;
 			}
-			
+
 			public boolean onEndWalk() {
-				if(Projekt.this.focus.dialog)
-					Projekt.this.focus.freeze=false;
+				if (Projekt.this.focus.dialog) {
+					Projekt.this.focus.freeze = false;
+					Projekt.this.focus.dialog = false;
+				}
 				return true;
 			}
-		  });
+		});
 	}
-	int delay=0;
+	int delay = 0;
+
 	/**
 	 * tick är medlem av Render och anropas varje loopintervall och här kollar vi då om det har tryckts ner någon tangent
 	 * @param k boolean[]
 	 */
 	@Override
-	public void tick(boolean[] k){
-		boolean left=(k[KeyEvent.VK_A]||k[KeyEvent.VK_LEFT]) && !focus.freeze;
-		boolean right=(k[KeyEvent.VK_D]||k[KeyEvent.VK_RIGHT]) && !focus.freeze;
-		boolean up=(k[KeyEvent.VK_W]||k[KeyEvent.VK_UP]) && !focus.freeze;
-		boolean down=(k[KeyEvent.VK_S]||k[KeyEvent.VK_DOWN]) && !focus.freeze;
-		if(k[KeyEvent.VK_ESCAPE]||this.exitCode==1){
-			this.exitCode=1;
+	public void tick(boolean[] k) {
+		boolean left = (k[Keys.left] || k[KeyEvent.VK_LEFT]) && !focus.freeze;
+		boolean right = (k[Keys.right] || k[KeyEvent.VK_RIGHT]) && !focus.freeze;
+		boolean up = (k[Keys.up] || k[KeyEvent.VK_UP]) && !focus.freeze;
+		boolean down = (k[Keys.down] || k[KeyEvent.VK_DOWN]) && !focus.freeze;
+		boolean a=(k[Keys.a]);
+		boolean b=(k[Keys.b]);
+		boolean select=(k[Keys.select]);
+		if (k[Keys.esc] || this.exitCode == 1) {
+			this.exitCode = 1;
 			return;
 		}
-		//**** pickup ****//
-		if(k[KeyEvent.VK_CONTROL])
-			 this.pickup();
-
-                //**** pickup ****//
-                if(k[KeyEvent.VK_CONTROL])
-                    this.pickup();
-		if (!(left || right || up || down)){
+		
+		if (!(left || right || up || down)) {
 			//**** Delayen ****//
 			delay++;
+		} else {
+			delay = 0;
 		}
-		else
-			delay=0;
-		if(delay>=radius)
-			this.focus.frame=0;
+		if (delay >= radius) {
+			this.focus.frame = 0;
+		}
+		if (k[Keys.a]) {
+			if (!Dialogs.endof)
+				this.drawDialog(Dialogs.nextMessage());
+			else
+				this.focus.onWalkCallback.onEndWalk();
+		}
+		
+		//**** pickup ****//
+		if (k[Keys.b]) {
+			this.pickup();
+		}
+		
 		//X-axeln
-		if(this.focus.y-this.focus.y2/radius==0&&!up&&!down){
-			if((left&&!right)&&(this.focus.x-this.focus.x2/radius>=0)&&focus.x>1&&this.world.canGo(this.focus.x-1, this.focus.y)){
+		if (this.focus.y - this.focus.y2 / radius == 0 && !up && !down) {
+			if ((left && !right) && (this.focus.x - this.focus.x2 / radius >= 0) && focus.x > 1 && this.world.canGo(this.focus.x - 1, this.focus.y)) {
 				focus.x--;
 			}
-			if((right&&!left)&&(this.focus.x-this.focus.x2/radius<=0)&&focus.x2<world.width*radius-radius&&this.world.canGo(this.focus.x+1, this.focus.y)){
+			if ((right && !left) && (this.focus.x - this.focus.x2 / radius <= 0) && focus.x2 < world.width * radius - radius && this.world.canGo(this.focus.x + 1, this.focus.y)) {
 				focus.x++;
 			}
 		}
 		//Y-axeln
-		if(this.focus.x-this.focus.x2/radius==0){
-			if((up&&!down)&&(this.focus.y-this.focus.y2/radius>=0)&&focus.y>1&&this.world.canGo(this.focus.x, this.focus.y-1)){
+		if (this.focus.x - this.focus.x2 / radius == 0) {
+			if ((up && !down) && (this.focus.y - this.focus.y2 / radius >= 0) && focus.y > 1 && this.world.canGo(this.focus.x, this.focus.y - 1)) {
 				focus.y--;
 			}
-			if((down&&!up)&&(this.focus.y-this.focus.y2/radius<=0)&&focus.y>=1&&focus.y2<world.height*radius-radius&&this.world.canGo(this.focus.x, this.focus.y+1)){
+			if ((down && !up) && (this.focus.y - this.focus.y2 / radius <= 0) && focus.y >= 1 && focus.y2 < world.height * radius - radius && this.world.canGo(this.focus.x, this.focus.y + 1)) {
 				focus.y++;
 			}
 		}
 		/* Directions!!!*/
-		if((this.focus.x)*radius-(this.focus.x2)==0){
-                        this.focus.stand=false;
-			if((left&&!right)){
-				focus.direciton=1;
+		if ((this.focus.x) * radius - (this.focus.x2) == 0) {
+			this.focus.stand = false;
+			if ((left && !right)) {
+				focus.direciton = 1;
 			}
-			if((right&&!left)){
-				focus.direciton=2;
+			if ((right && !left)) {
+				focus.direciton = 2;
 			}
 		}
-		if((this.focus.y)-(this.focus.y2)/radius==0){
-			if((up&&!down)){
-				focus.direciton=3;
+		if ((this.focus.y) - (this.focus.y2) / radius == 0) {
+			if ((up && !down)) {
+				focus.direciton = 3;
 			}
-			if((down&&!up)){
-				focus.direciton=0;
+			if ((down && !up)) {
+				focus.direciton = 0;
 			}
 		}
 	}
-	private void pickup() {
-		int direction=this.focus.direciton;
-		int x=0,y=0;
-		if(direction==1||direction==2)
-			x=(int)direction*2-3;
-		else if(direction==3||direction==0)
-			y=(-2*direction)/3+1;
-		Color temp = this.world.getRGBA((int)this.focus.x2/radius+x, (int) this.focus.y2/radius+y);
-		if(!(temp.equals(new Color(0xff000000))||temp.equals(new Color(0xffffffff)))){
-			System.out.println(Dialogs.hello.sayHello);
-			this.focus.dialog=true;
-			this.focus.freeze=true;
-			Graphics2D ag =this.world.alpha.createGraphics();
-			ag.setColor(Color.white);
-			ag.fillRect((int)this.focus.x2/radius+x, (int)this.focus.y2/radius+y, 1, 1);
 
-			ag = this.world.items.createGraphics ();
+	private void pickup() {
+		int direction = this.focus.direciton;
+		int x = 0, y = 0;
+		if (direction == 1 || direction == 2) {
+			x = (int) direction * 2 - 3;
+		} else if (direction == 3 || direction == 0) {
+			y = (-2 * direction) / 3 + 1;
+		}
+		Color temp = this.world.getRGBA((int) this.focus.x2 / radius + x, (int) this.focus.y2 / radius + y);
+		if (!(temp.equals(new Color(0xff000000)) || temp.equals(new Color(0xffffffff)))) {
+			this.focus.dialog = true;
+			this.focus.freeze = true;
+			Graphics2D ag = this.world.alpha.createGraphics();
+			ag.setColor(Color.white);
+			ag.fillRect((int) this.focus.x2 / radius + x, (int) this.focus.y2 / radius + y, 1, 1);
+
+			ag = this.world.items.createGraphics();
 			AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.CLEAR, 0.0f);
 			Composite c = ag.getComposite();
 			ag.setComposite(composite);
-			ag.setColor(new Color(0, 0, 0,0));
-			ag.fillRect(((int)this.focus.x2/radius+x)*radius, ((int)this.focus.y2/radius+y)*radius, radius, radius);
+			ag.setColor(new Color(0, 0, 0, 0));
+			ag.fillRect(((int) this.focus.x2 / radius + x) * radius, ((int) this.focus.y2 / radius + y) * radius, radius, radius);
 			ag.setComposite(c);
-			int b=temp.getBlue();
-			if(b!=0) this.focus.addItem(b);
+			int b = temp.getBlue();
+			if (b != 0) {
+				this.focus.addItem(b);
+			}
 		}
 	}
+
 	/**
 	 *  Denna funktionen ritar allt i hela spelet (inklusive det world genererar) som en bild
 	 * @param g Graphics
 	 */
 	@Override
-	public void paint(Graphics g){
-		if((focus.x*focus.radius!=focus.x2))
+	public void paint(Graphics g) {
+		if ((focus.x * focus.radius != focus.x2)) {
 			focus.slowMove(0);
-		else if(focus.y*focus.radius!=focus.y2)
+		} else if (focus.y * focus.radius != focus.y2) {
 			focus.slowMove(1);
-		if(world.isPoortal((int)(this.focus.x2/radius-0.1+1), (int)(this.focus.y2/radius-0.1+1))){
+		}
+		if (world.isPoortal((int) (this.focus.x2 / radius - 0.1 + 1), (int) (this.focus.y2 / radius - 0.1 + 1))) {
 			/** @TODO switch worlds!!! */
-                        focus.freeze = true;
-			try{
+			focus.freeze = true;
+			try {
 				try {
 					this.focus.addWorld(this.world.copy());
 				} catch (CloneNotSupportedException ex) {
 					//Logger.getLogger(Projekt.class.getName()).log(Level.SEVERE, null, ex);
 				}
-				String pa=getClass().getResource("/res/worlds/world"+this.world.getRGBA(this.world.pos[0], this.world.pos[1]).getRed()).getPath();
-				if(this.focus.getWorldFromPath(pa)==-1)
+				String pa = getClass().getResource("/res/worlds/world" + this.world.getRGBA(this.world.pos[0], this.world.pos[1]).getRed()).getPath();
+				if (this.focus.getWorldFromPath(pa) == -1) {
 					this.world.setWorldFromColor("/res/worlds");
-				else
-					this.world=this.focus.getWorld(this.focus.getWorldFromPath(pa));
-				if(this.world.canGo(this.focus.y+1, this.focus.x))
+				} else {
+					this.world = this.focus.getWorld(this.focus.getWorldFromPath(pa));
+				}
+				if (this.world.canGo(this.focus.y + 1, this.focus.x)) {
 					this.focus.y++;
-				else if(this.world.canGo(this.focus.y-1, this.focus.x))
+				} else if (this.world.canGo(this.focus.y - 1, this.focus.x)) {
 					this.focus.y--;
-				else if(this.world.canGo(this.focus.y, this.focus.x-1))
+				} else if (this.world.canGo(this.focus.y, this.focus.x - 1)) {
 					this.focus.x--;
-				else if(this.world.canGo(this.focus.y, this.focus.x+1))
+				} else if (this.world.canGo(this.focus.y, this.focus.x + 1)) {
 					this.focus.x++;
+				}
+			} catch (ArrayIndexOutOfBoundsException b) {
+				this.exitCode = 1;
 			}
-			catch(ArrayIndexOutOfBoundsException b){this.exitCode=1;}
 		}
-		world.paint(g,(int)this.focus.x2,(int)this.focus.y2,this.getWidth(),this.getHeight());
-		BufferedImage t = focus.c.getSubimage(radius*(((int)focus.frame)%4), 20*focus.direciton, radius, 20);
-		g.drawImage(t,this.getWidth()/2-t.getWidth()/2-radius/2+radius, this.getHeight()/2-t.getHeight()-radius/5+radius, this);
-		world.paintTop(g,(int)this.focus.x2,(int)this.focus.y2,this.getWidth(),this.getHeight());
-		if(this.focus.dialog){
+		world.paint(g, (int) this.focus.x2, (int) this.focus.y2, this.getWidth(), this.getHeight());
+		BufferedImage t = focus.c.getSubimage(radius * (((int) focus.frame) % 4), 20 * focus.direciton, radius, 20);
+		g.drawImage(t, this.getWidth() / 2 - t.getWidth() / 2 - radius / 2 + radius, this.getHeight() / 2 - t.getHeight() - radius / 5 + radius, this);
+		world.paintTop(g, (int) this.focus.x2, (int) this.focus.y2, this.getWidth(), this.getHeight());
+		if (this.focus.dialog) {
 			g.setColor(new Color(0x666666));
-			g.setFont(new Font("Lucida Typewriter Regular",Font.BOLD, 12));
-			this.drawShadowWithString("Version: \u03B1 0.1", 2, 12, Color.white, new Color(0x666666));
-			this.drawDialog("hejjkjlkjlkfjewlkdwjflkdjflksjdflkjdsflkjdslkfjdslkfjdslkfjlkdsjflkdsjfkldsjflkjdskfjkdjflkds");
+			g.setFont(new Font("Lucida Typewriter Regular", Font.BOLD, 12));
+			this.drawShadowWithString("Version: \u03B1 0.2", 2, 12, Color.white, new Color(0x666666));
+			Dialogs.initDialog(Dialogs.Begin.sayHello);
+			this.drawDialog(Dialogs.message[0]);
 		}
 	}
-	public void drawDialog(String message){
-		int b=5;
-		int m=18;
-		int y=this.getHeight()/4;
-		this.dbg.fillRect(0, y*3, this.getWidth(), y+this.getHeight()%4);
+
+	public void drawDialog(String message) {
+		int b = 5;
+		int m = 18;
+		int y = this.getHeight() / 4;
+		this.dbg.fillRect(0, y * 3, this.getWidth(), y + this.getHeight() % 4);
 		this.dbg.setColor(Color.black);
-		this.dbg.drawRect(b, y*3+b, this.getWidth()-2*b, y+this.getHeight()%4-2*b);
-		
-              this.dbg.setFont(new Font(Font.DIALOG,Font.BOLD, 10));
-		String[] lines=message.split("\n");
-		for(int i=0;i<lines.length;i++){
+		this.dbg.drawRect(b, y * 3 + b, this.getWidth() - 2 * b, y + this.getHeight() % 4 - 2 * b);
+
+		this.dbg.setFont(new Font(Font.DIALOG, Font.BOLD, 10));
+		String[] lines = message.split("\n");
+		for (int i = 0; i < lines.length; i++) {
 			FontMetrics fm = this.getFontMetrics(this.dbg.getFont());
 			int width = fm.stringWidth(lines[i]);
-			if(width<this.getWidth()-(2*b))
-				this.drawShadowWithString(lines[i],b+b, 3*y+b+m+i*(12),new Color(0x222222), new Color(0xeeeeee));
+			if (width < this.getWidth() - (2 * b)) {
+				this.drawShadowWithString(lines[i], b + b, 3 * y + b + m + i * (12), new Color(0x222222), new Color(0xeeeeee));
+			}
 		}
 	}
-        public void drawShadowWithString(String string, int x, int y, Color c1, Color c2){
+
+	public void drawShadowWithString(String string, int x, int y, Color c1, Color c2) {
 		this.dbg.setColor(c2);
-		this.dbg.drawString(string, x+1, y);
-		this.dbg.drawString(string, x-1, y);
-		this.dbg.drawString(string, x, y+1);
-		this.dbg.drawString(string, x, y-1);
+		this.dbg.drawString(string, x + 1, y);
+		this.dbg.drawString(string, x - 1, y);
+		this.dbg.drawString(string, x, y + 1);
+		this.dbg.drawString(string, x, y - 1);
 		this.dbg.setColor(c1);
 		this.dbg.drawString(string, x, y);
-        }
+	}
 }
